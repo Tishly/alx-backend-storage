00901291a6f418e9c5d2450fa040b2e8d9e72879
Tishly <ekaetteessien27@gmail.com>
2023-01-18 05:43:21 +0100

    Search for specific data

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+"""MongoDB query using PyMongo"""
 
 def schools_by_topic(mongo_collection, topic):
+    """Searches the database for schools based on topic"""
     return mongo_collection.find({"topics": topic})
