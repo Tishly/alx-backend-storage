ea651f3ac1fea0c679e2f0fc888894ed4296db91
Tishly <ekaetteessien27@gmail.com>
2023-01-18 23:55:40 +0100

    Creating class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python file to test redis"""
+import redis
+
+Class Cache():
+    def __init__(self):
+        _redis = redis.Redis()
+        _redis.flushdb
+
+    def store(data):
+        pass
