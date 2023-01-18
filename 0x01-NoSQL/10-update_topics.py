d6b592559a27703c3e3c56862576a4e48ee831be
Tishly <ekaetteessien27@gmail.com>
2023-01-18 05:32:55 +0100

    Updates a document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""MongoDB query using PyMongo"""
+
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates attributes already in the database"""
+    return mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
