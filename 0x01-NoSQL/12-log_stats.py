5817be4952b9425ca126b7a497d003cb8d077d30
Tishly <ekaetteessien27@gmail.com>
2023-02-15 00:30:02 +0100

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-
+Log stats from a dump file
 """
 from mongodb import MongoClient 
 
 
 with MongoClient as client:
+    """Getting log stats from a dump file"""
     logs.nginx.aggregate([
         { $group:  {x: {$sum: "logs"}}
             },
