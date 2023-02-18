d16a9e4e14d2a41e5a30d843ce69074bd7c4bead
Tishly <ekaetteessien27@gmail.com>
2023-02-18 05:29:36 +0100

    implemented set creation with redis client

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,15 +5,16 @@ import uuid
 from typing import Union
 
 
-Class Cache():
+class Cache():
     """Redis in Python script"""
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
-    def store(data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: (Union[str, bytes, int, float])) -> str:
         """
         Generates a random key, store the input data in Redis using the
         random key and return the key"""
-        key = uuid.uuid5(data)
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
         return key
