4742e0b97267335ece0ecc59d47ee4d3960e29e6
Tishly <ekaetteessien27@gmail.com>
2023-02-18 05:09:31 +0100

    created key with uuid

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """Python file to test redis"""
 import redis
+import uuid
+from typing import Union
+
 
 Class Cache():
     """Redis in Python script"""
     def __init__(self):
-        _redis = redis.Redis()
-        _redis.flushdb
+        self._redis = redis.Redis()
+        self._redis.flushdb
 
     def store(data: Union[str, bytes, int, float]) -> str:
         """
-        Stores data with random uuid
-        """
-        pass
+        Generates a random key, store the input data in Redis using the
+        random key and return the key"""
+        key = uuid.uuid5(data)
+        return key
